3f465bc426132f7764077948b57fe396066d8902
Alvaro Hernandez <143532029+avsh1@users.noreply.github.com>
2024-08-12 15:59:25 -0600

    Add files via upload

---
+++ Sistema-de-gestion-de-libros/src/main/java/org/esfe/SistemaDeGestionDeLibrosApplication.java
@@ -0,0 +1,13 @@
+package org.esfe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeGestionDeLibrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeGestionDeLibrosApplication.class, args);
+	}
+
+}
